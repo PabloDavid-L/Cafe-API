00a781632774afaa439e86e315566a60c1653fc3
PabloDavid-L <davidlabala9999@hotmail.com>
2025-10-16 21:25:41 -0300

    feat: add Cafe and Tipo entities with TypeORM integration
    - Updated package.json to include TypeORM and pg dependencies.
    - Created Cafe entity with properties: id, name, description, and a ManyToOne relationship with Tipo.
    - Created Tipo entity with properties: id, name, description, and a OneToMany relationship with Cafe.
    - Added docker-compose.yml for PostgreSQL setup with persistent storage.

--- src/cafes/entities/cafe.entity.ts
+++ src/cafes/entities/cafe.entity.ts
@@ -1,1 +1,14 @@
-export class Cafe {}
+import { Tipo } from "src/tipos/entities/tipo.entity";
+import { Column, Entity, ManyToOne, PrimaryGeneratedColumn } from "typeorm";
+
+@Entity()
+export class Cafe {
+    @PrimaryGeneratedColumn()
+    id: number;
+    @Column()
+    name: string;
+    @Column()
+    description: string;
+    @ManyToOne(() => Tipo, (tipo) => tipo.cafes, {eager: true})
+    tipo: Tipo;
+}
--- src/tipos/entities/tipo.entity.ts
+++ src/tipos/entities/tipo.entity.ts
@@ -1,1 +1,14 @@
-export class Tipo {}
+import { Cafe } from "src/cafes/entities/cafe.entity";
+import { Column, Entity, OneToMany, PrimaryColumn, PrimaryGeneratedColumn } from "typeorm";
+
+@Entity()
+export class Tipo {
+    @PrimaryGeneratedColumn()
+    id: number;
+    @Column()
+    name: string;
+    @Column()
+    description: string;
+    @OneToMany(() => Cafe, (cafe) => cafe.tipo, {eager: true})
+    cafes: Cafe[];
+}

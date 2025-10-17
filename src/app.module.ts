9c3400b58d89cc6dc6d77a7d02014afd5de7d437
PabloDavid-L <davidlabala9999@hotmail.com>
2025-10-16 23:11:11 -0300

    com

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,23 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TiposModule } from './tipos/tipos.module';
 import { CafesModule } from './cafes/cafes.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [TiposModule, CafesModule],
+  imports: [
+    TiposModule,
+    CafesModule,
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'postgres',
+      password: 'postgres',
+      database: 'cafedb',
+      autoLoadEntities: true,
+      synchronize: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/cafes/cafes.module.ts
+++ src/cafes/cafes.module.ts
@@ -3,6 +3,7 @@ import { CafesService } from './cafes.service';
 import { CafesController } from './cafes.controller';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Cafe])],
   controllers: [CafesController],
   providers: [CafesService],
 })

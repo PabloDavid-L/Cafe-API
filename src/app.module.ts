ffa9ab9692e26667b2e8c654efd2b980999c4bbc
PabloDavid-L <davidlabala9999@hotmail.com>
2025-10-16 21:04:57 -0300

    feat: implement cafes and tipos modules with controllers, services, and DTOs

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TiposModule } from './tipos/tipos.module';
+import { CafesModule } from './cafes/cafes.module';
 
 @Module({
-  imports: [],
+  imports: [TiposModule, CafesModule],
   controllers: [AppController],
   providers: [AppService],
 })
